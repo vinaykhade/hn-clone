6cb5fa580cfa976b2649b21c75dd425519ec063a
vnaydotk <vinay.khade@happay.in>
2020-04-20 23:42:56 +0530

    Updates

---
+++ precache-manifest.9400f61b93b801ee874aae03608e3590.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2677d8140f30823e708c51ef701c5ced",
+    "url": "/hn-clone/index.html"
+  },
+  {
+    "revision": "c5c77089d992ab4dabdb",
+    "url": "/hn-clone/static/js/2.80f7f2c4.chunk.js"
+  },
+  {
+    "revision": "7c93b955218104b209c51bc495b643a6",
+    "url": "/hn-clone/static/js/2.80f7f2c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b4ede92e30da731fbf6",
+    "url": "/hn-clone/static/js/main.806a180e.chunk.js"
+  },
+  {
+    "revision": "459886f8b84c0e74fe21",
+    "url": "/hn-clone/static/js/runtime-main.67c9952b.js"
+  }
+]);
