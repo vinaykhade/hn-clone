767c93b6f42c389099e7854d51b869618c91a90d
vnaydotk <vinay.khade@happay.in>
2020-04-20 22:48:36 +0530

    added list component and styles

---
+++ src/components/List/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ListItem from '../ListItem';
+
+import history from '../../utils/history';
+
+import * as Styled from './styles';
+
+class List extends Component {
+  static propTypes = {
+    stories: PropTypes.array.isRequired,
+  };
+
+  render() {
+    const { stories, pageNum, upvote, hiddenFeeds } = this.props;
+    return (
+      <Styled.ListWrapper>
+        {
+        stories
+        .filter(story => hiddenFeeds.indexOf(story.objectID) === -1)
+        .map((story,index) => (
+          <ListItem 
+            key={story.objectID} 
+            index={index} 
+            upvoted={upvote[story.objectID]}
+            pageNum={pageNum} 
+            pageSize={stories.length}
+            {...this.props}
+            {...story} 
+          />
+        ))}
+        <Styled.MoreBtn onClick={() => history.push(`/news/page/${pageNum+1}`)}>More</Styled.MoreBtn>
+      </Styled.ListWrapper>
+    );
+  }
+}
+
+export default List;
---
+++ src/components/List/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const ListWrapper = styled.ul`
+  background-color: ${({ theme }) => theme.backgroundSecondary};
+  border-radius: 4px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const MoreBtn = styled.span`
+  display: block;
+  margin: 10px 0;
+  color: black;
+  text-align: center;
+  background: orange;
+  font-size: 14px;
+  padding: 10px 20px;
+  border-radius: 3px;
+  width: 80px;
+  cursor: pointer;
+`
